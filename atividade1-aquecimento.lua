925f23d0d97e600ba6ed2f237c2b0aec0011243c
KSalmaze <pedrohsalmaze@gmail.com>
2025-03-17 14:24:22 -0300

    Versão final otimizada

--- atividade1-aquecimento.lua
+++ atividade1-aquecimento.lua
@@ -1,26 +1,17 @@
-local line = io.read()
-local n, s = line:match("(%d+)%s+(%d+)")
+local n, s = io.read():match("(%d+)%s+(%d+)")
 n = tonumber(n)
 s = tonumber(s)
-
-local input = io.read()
 
 local dictionary = {}
 local counter = 0
 
-for element in input:gmatch("%S+") do
+for element in io.read():gmatch("%S+") do
     local temp = tonumber(element)
 
     if temp < s then
-        if dictionary[math.floor(s - temp)] then
-            counter = counter + dictionary[s - temp]
-        end
-    
-        if dictionary[temp] then 
-            dictionary[temp] = dictionary[temp] + 1
-        else
-            dictionary[temp] = 1
-        end
+
+        counter = counter + (dictionary[s-temp] or 0)
+        dictionary[temp] = (dictionary[temp] or 0) + 1
     end
 end
 
