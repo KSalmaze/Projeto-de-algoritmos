c7cc41e1de3895770a2e138c827df4fd02a4e34d
KSalmaze <pedrohsalmaze@gmail.com>
2025-03-17 20:07:44 -0300

    Primeira versao funcional

--- atividade2-stl1.lua
+++ atividade2-stl1.lua
@@ -1,20 +1,29 @@
 local t = tonumber(io.read())
-local n = tonumber(io.read())
-
-print(t)
-print(n)
 
 for i=1, t do
+    local n = tonumber(io.read())
 
-    -- dicionario de assuntos
+    local dictionary = {}
 
     for j=1, n do
         local a, b = io.read():match("(%S+)%s+(%d+)")
-        print(a)
-        print()
+        dictionary[a] = tonumber(b)
     end
 
-    local d = tonumber(io.read())
+    local d = tonumber(io.read()) -- tempo para fazer a atividade
+    local topic = io.read()
+
+    local time_taken = (dictionary[topic] or 100)
+
+    if d >= time_taken then
+        print("Case " .. i .. ": Ufa!")
+    else
+        if time_taken <= d + 5 then
+            print("Case " .. i .. ": Atrasado")
+        else
+            print("Case " .. i .. ": Deu ruim! Va trabalhar")
+        end
+    end
 
     -- processamento dos prazos
 end
