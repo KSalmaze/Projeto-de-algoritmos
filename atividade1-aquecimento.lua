9f4bed311bb3f988c3825f6b8b59b6e4b15f2e7a
KSalmaze <pedrohsalmaze@gmail.com>
2025-03-17 19:18:26 -0300

    Enunciado adicionado

--- atividade1-aquecimento.lua
+++ atividade1-aquecimento.lua
@@ -1,3 +1,13 @@
+--[[ Enunciado
+Dado um vetor v de n inteiros (1 ≤ n ≤ 106) e um inteiro s, calcule a quantidade de pares (vi
+, vj ),1 ≤ i, j, ≤ n, de elementos do vetor cuja soma ´e s. Nota: 1 ≤ vi ≤ 10000 e 1 ≤ s ≤ 10000
+
+A primeira linha da entrada cont´em os valores de n e s, separados por um espa¸co em branco. Na
+linha subsequente, os n inteiros separados por um espa¸co em branco.
+
+Imprima, em uma ´unica linha, a quantidade de pares encontrada.
+]]
+
 local n, s = io.read():match("(%d+)%s+(%d+)")
 n = tonumber(n)
 s = tonumber(s)
