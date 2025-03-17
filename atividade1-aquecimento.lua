f249e16f5f44b5d02b662a03143e02190b5ea3fd
KSalmaze <pedrohsalmaze@gmail.com>
2025-03-17 11:40:08 -0300

    Projeto inicial do algoritmo

--- atividade1-aquecimento.lua
+++ atividade1-aquecimento.lua
@@ -8,9 +8,22 @@ print(n .. "-" .. s)
 local input = io.read()
 
 local vector = {}
+-- Dicionario de Numero (menor q o S) -> Quantas vezes apareceu
+-- Contador de pares = 0
 
 for element in input:gmatch("%S+") do
     print(element)
+    -- Se o elemento for maior que o numero
+    --    proxima repeticao
+
+    -- Se dicionario.contains(S - element) 
+    --    pares += dicionario[S - element]
+
+    -- Se dicionario.contains(S)
+    --    dicionario[S]++
+    -- Se nao
+    --    dicionario.add(element, 1)
+
 end
 
 print("End")
